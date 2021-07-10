2ced0e5cec0a4462393194aed1eef89a3c559cb2
Cindy <r0767301@student.thomasmore.be>
2021-07-10 20:34:10 +0200

    rol model en user model aangemaakt + ingevuld

---
+++ src/app/roles/models/role.model.ts
@@ -0,0 +1,8 @@
+export class Role {
+    RoleID: number;
+    Name: string;
+    constructor(RoleID: number, Name: string){
+		this.RoleID = RoleID;
+		this.Name = Name;
+		} 
+}
--- src/app/users/models/user.model.ts
+++ src/app/users/models/user.model.ts
@@ -1,2 +1,29 @@
+import { Role } from '../../roles/models/role.model'; 
+
 export class User {
+    UserID: number;
+	FirstName: string;
+    LastName: string;
+	Email: string;
+	Username: string;
+	Password: string;
+	FotoURL: string;
+	Function: string;
+    LinkedInURL: string;
+    RoleID: number;
+    Role: Role;
+
+	 constructor(UserID: number, FirstName: string, LastName: string, Email: string, Username: string, Password: string, FotoURL: string, Function: string, LinkedInURL:string, RoleID: number, Role: Role){
+		this.UserID = UserID;
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.Email = Email;
+		this.Username = Username;
+		this.Password = Password;
+		this.FotoURL = FotoURL;
+        this.Function = Function;
+        this.LinkedInURL = LinkedInURL;
+        this.RoleID = RoleID;
+        this.Role = Role;
+		} 
 }
